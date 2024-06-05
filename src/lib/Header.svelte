219065b3b0d6d4e2b2d18fd17fbb70281f25af76
Jane W Hsieh <jhsieh2@andrew.cmu.edu>
2024-06-04 21:40:13 -0400

    initializing login

---
+++ src/lib/Header.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+	import { signOut } from 'firebase/auth';
+	import { auth } from '$lib/firebase.client';
+	import { authUser } from '$lib/authstore';
+
+	const handleLogout = () => {
+		signOut(auth)
+			.then(() => {
+				$authUser = undefined;
+				goto('/login');
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+</script>
+
+<header class="space-y-4">
+	<a href="/" class="font-bold hover:underline">Home</a>
+
+	<nav class="flex gap-4">
+		{#if $authUser}
+			<a
+				href="/protected"
+				class="hover:underline"
+				class:active={$page.url.pathname === '/protected'}>Protected</a
+			>
+			<button class="hover:underline" on:click={handleLogout}>Logout</button>
+		{:else}
+			<a href="/register" class="hover:underline" class:active={$page.url.pathname === '/register'}
+				>Register</a
+			>
+			<a href="/login" class="hover:underline" class:active={$page.url.pathname === '/login'}
+				>Login</a
+			>
+		{/if}
+	</nav>
+</header>
+
+<style lang="postcss">
+	nav a.active {
+		@apply font-bold text-indigo-700;
+	}
+</style>
