7f36e93806a275019145bd1edde2041a24f4b1ef
Sajel Surati <sajelsurati@gmail.com>
2024-06-07 11:43:56 -0400

    added demographics

--- src/routes/protected/+page.svelte
+++ src/routes/protected/+page.svelte
@@ -1,5 +1,12 @@
+<script>
+
+    import { page } from "$app/stores";
+
+</script>
 <svelte:head>
 	<title>Protected route</title>
 </svelte:head>
 
 <h1 class="text-4xl font-bold">Protected route</h1>
+<a href = "/protected/demographics" class="hover:underline" class:active={$page.url.pathname === '/protected/demographics'}>
+	Change My Demographics</a>
