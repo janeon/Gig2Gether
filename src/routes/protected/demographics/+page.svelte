43801ab7e616ad0733a24a08292f44ea484b9a0c
Sajel Surati <sajelsurati@gmail.com>
2024-06-07 11:43:56 -0400

    added demographics

---
+++ src/routes/protected/demographics/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let age: number;
+    let gender: string;
+    let race: string[];
+    let ethnicity : string;
+    let householdIncome : number; //Unsure what this value should be
+    let w2Hours : number;
+    let otherGigHours : number;
+</script>
