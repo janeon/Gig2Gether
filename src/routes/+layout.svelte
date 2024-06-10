4dbc2bc6d8a98bef51207e6ce3da51b408e18a75
Jane Hsieh <janeon@ALEECIAMACM1.ISRI.CMU.EDU>
2024-06-10 00:48:57 -0500

    configuring tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-	// import '$styles/app.css';
-
+	import "../app.css";
 	import Header from '$lib/Header.svelte';
 </script>
-
+  
 <main class="p-8 space-y-12 font-serif">
 	<Header />
 	<slot />
