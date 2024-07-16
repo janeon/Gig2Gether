85be74249dca2189efd870899d8f06b564bd5557
Sajel Surati <sajelsurati@gmail.com>
2024-07-16 13:17:06 -0400

    Started developing share

---
+++ src/routes/protected/(share)/sharestrategy/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import ShareTagSelect from "$lib/components/ShareTagSelect.svelte";
+
+    $: tags = []
+</script>
+
+<ShareTagSelect tags={tags} platform={"rover"} />
+<p>{tags}</p>
