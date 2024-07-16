6fd8b980dcf93fa6f0a4ef714281765e3ca39c87
Scarlett <scarlett@scarlettdeair.mynetworksettings.com>
2024-07-15 22:20:55 -0400

    The first version of Splash Page

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -26,10 +26,6 @@
  * ```
  */
 declare module '$env/static/private' {
-	export const VITE_FIREBASE_API_KEY: string;
-	export const VITE_FIREBASE_APP_ID: string;
-	export const VITE_FIREBASE_AUTH_DOMAIN: string;
-	export const VITE_FIREBASE_PROJECT_ID: string;
 	export const MANPATH: string;
 	export const TERM_PROGRAM: string;
 	export const NODE: string;
@@ -39,7 +35,9 @@ declare module '$env/static/private' {
 	export const HOMEBREW_REPOSITORY: string;
 	export const TMPDIR: string;
 	export const npm_config_global_prefix: string;
+	export const CONDA_SHLVL: string;
 	export const TERM_PROGRAM_VERSION: string;
+	export const CONDA_PROMPT_MODIFIER: string;
 	export const ZDOTDIR: string;
 	export const ORIGINAL_XDG_CURRENT_DESKTOP: string;
 	export const MallocNanoZone: string;
@@ -49,16 +47,18 @@ declare module '$env/static/private' {
 	export const USER: string;
 	export const COMMAND_MODE: string;
 	export const npm_config_globalconfig: string;
+	export const CONDA_EXE: string;
 	export const SSH_AUTH_SOCK: string;
 	export const __CF_USER_TEXT_ENCODING: string;
 	export const npm_execpath: string;
+	export const _CE_CONDA: string;
 	export const PATH: string;
 	export const npm_package_json: string;
 	export const _: string;
-	export const LaunchInstanceID: string;
 	export const npm_config_userconfig: string;
 	export const npm_config_init_module: string;
 	export const USER_ZDOTDIR: string;
+	export const CONDA_PREFIX: string;
 	export const __CFBundleIdentifier: string;
 	export const npm_command: string;
 	export const PWD: string;
@@ -71,6 +71,7 @@ declare module '$env/static/private' {
 	export const XPC_FLAGS: string;
 	export const npm_config_node_gyp: string;
 	export const npm_package_version: string;
+	export const _CE_M: string;
 	export const XPC_SERVICE_NAME: string;
 	export const VSCODE_INJECTION: string;
 	export const SHLVL: string;
@@ -78,15 +79,16 @@ declare module '$env/static/private' {
 	export const VSCODE_GIT_ASKPASS_MAIN: string;
 	export const HOMEBREW_PREFIX: string;
 	export const npm_config_cache: string;
+	export const CONDA_PYTHON_EXE: string;
 	export const LOGNAME: string;
 	export const npm_lifecycle_script: string;
 	export const VSCODE_GIT_IPC_HANDLE: string;
+	export const CONDA_DEFAULT_ENV: string;
 	export const npm_config_user_agent: string;
 	export const VSCODE_GIT_ASKPASS_NODE: string;
 	export const GIT_ASKPASS: string;
 	export const INFOPATH: string;
 	export const HOMEBREW_CELLAR: string;
-	export const SECURITYSESSIONID: string;
 	export const npm_node_execpath: string;
 	export const npm_config_prefix: string;
 	export const COLORTERM: string;
@@ -122,10 +124,6 @@ declare module '$env/static/public' {
  */
 declare module '$env/dynamic/private' {
 	export const env: {
-		VITE_FIREBASE_API_KEY: string;
-		VITE_FIREBASE_APP_ID: string;
-		VITE_FIREBASE_AUTH_DOMAIN: string;
-		VITE_FIREBASE_PROJECT_ID: string;
 		MANPATH: string;
 		TERM_PROGRAM: string;
 		NODE: string;
@@ -135,7 +133,9 @@ declare module '$env/dynamic/private' {
 		HOMEBREW_REPOSITORY: string;
 		TMPDIR: string;
 		npm_config_global_prefix: string;
+		CONDA_SHLVL: string;
 		TERM_PROGRAM_VERSION: string;
+		CONDA_PROMPT_MODIFIER: string;
 		ZDOTDIR: string;
 		ORIGINAL_XDG_CURRENT_DESKTOP: string;
 		MallocNanoZone: string;
@@ -145,16 +145,18 @@ declare module '$env/dynamic/private' {
 		USER: string;
 		COMMAND_MODE: string;
 		npm_config_globalconfig: string;
+		CONDA_EXE: string;
 		SSH_AUTH_SOCK: string;
 		__CF_USER_TEXT_ENCODING: string;
 		npm_execpath: string;
+		_CE_CONDA: string;
 		PATH: string;
 		npm_package_json: string;
 		_: string;
-		LaunchInstanceID: string;
 		npm_config_userconfig: string;
 		npm_config_init_module: string;
 		USER_ZDOTDIR: string;
+		CONDA_PREFIX: string;
 		__CFBundleIdentifier: string;
 		npm_command: string;
 		PWD: string;
@@ -167,6 +169,7 @@ declare module '$env/dynamic/private' {
 		XPC_FLAGS: string;
 		npm_config_node_gyp: string;
 		npm_package_version: string;
+		_CE_M: string;
 		XPC_SERVICE_NAME: string;
 		VSCODE_INJECTION: string;
 		SHLVL: string;
@@ -174,15 +177,16 @@ declare module '$env/dynamic/private' {
 		VSCODE_GIT_ASKPASS_MAIN: string;
 		HOMEBREW_PREFIX: string;
 		npm_config_cache: string;
+		CONDA_PYTHON_EXE: string;
 		LOGNAME: string;
 		npm_lifecycle_script: string;
 		VSCODE_GIT_IPC_HANDLE: string;
+		CONDA_DEFAULT_ENV: string;
 		npm_config_user_agent: string;
 		VSCODE_GIT_ASKPASS_NODE: string;
 		GIT_ASKPASS: string;
 		INFOPATH: string;
 		HOMEBREW_CELLAR: string;
-		SECURITYSESSIONID: string;
 		npm_node_execpath: string;
 		npm_config_prefix: string;
 		COLORTERM: string;
