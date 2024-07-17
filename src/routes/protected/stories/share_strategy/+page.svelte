a440e5d55862e0873c91f989739dce2c9b093e0f
Sajel Surati <sajelsurati@gmail.com>
2024-07-17 12:22:28 -0400

    Added image/video upload, changed routing, added sidebar

--- src/routes/protected/stories/share_strategy/+page.svelte
+++ src/routes/protected/stories/share_strategy/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import ShareTagSelect from "$lib/components/ShareTagSelect.svelte";
-    import ShareVideo from "$lib/components/ShareVideo.svelte";
+    import ShareContent from "$lib/components/ShareContent.svelte";
 
     $: tags = []
     $: url = ""
@@ -13,5 +13,5 @@
 {#if tagSelection}
     <ShareTagSelect bind:tags={tags} platform={"rover"} bind:title={title} bind:description={description} type={'strategy'} bind:url={url} bind:tagSelection={tagSelection}/>
 {:else}
-    <ShareVideo bind:title={title} bind:tagSelection={tagSelection} bind:description={description} bind:url={url} />
+    <ShareContent bind:title={title} bind:tagSelection={tagSelection} bind:description={description} bind:url={url} />
 {/if}
