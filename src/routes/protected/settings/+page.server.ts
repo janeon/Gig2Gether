3212c40cb0865759176c58efef75dbeeebd357f6
Sajel Surati <sajelsurati@gmail.com>
2024-07-18 09:56:34 -0400

    Removed redirect

--- src/routes/protected/settings/+page.server.ts
+++ src/routes/protected/settings/+page.server.ts
@@ -6,5 +6,5 @@ export const load: PageServerLoad = async () => {
     // we only use this endpoint for the api
     // and don't need to see the page
     console.log("loading")
-    redirect(302, '/protected/settings/account')
+    // redirect(302, '/protected/settings/account')
   }
