02ad8ec5f420ea32d84182b310eeca09bc185344
Sajel Surati <sajelsurati@gmail.com>
2024-07-22 12:23:43 -0400

    Made width of sidebar to take up 1/4 of space -- can change later!

--- src/lib/components/SettingsSidebar.svelte
+++ src/lib/components/SettingsSidebar.svelte
@@ -5,15 +5,17 @@
     $: activeUrl = $page.url.pathname;
 </script>
 
-<Sidebar {activeUrl}>
-    <SidebarWrapper>
-        <SidebarGroup>
-            <SidebarItem label="My Account" href="/protected/settings/account"/>
-            <SidebarItem label="My Worker Profile" href="/protected/settings/profile"/>
-            <SidebarItem label="Demographics" href="/protected/settings/demographics"/>
-            <SidebarItem label="Sharing Preferences" href="/protected/settings/sharing_preferences"/>
-            <SidebarItem label="Withdraw Data"/>
-            <SidebarItem label="Notification"/>
-          </SidebarGroup>
-    </SidebarWrapper>
-</Sidebar>
+<div class="w-1/4">
+    <Sidebar {activeUrl}>
+        <SidebarWrapper>
+            <SidebarGroup>
+                <SidebarItem label="My Account" href="/protected/settings/account"/>
+                <SidebarItem label="My Worker Profile" href="/protected/settings/profile"/>
+                <SidebarItem label="Demographics" href="/protected/settings/demographics"/>
+                <SidebarItem label="Sharing Preferences" href="/protected/settings/sharing_preferences"/>
+                <SidebarItem label="Withdraw Data"/>
+                <SidebarItem label="Notification"/>
+              </SidebarGroup>
+        </SidebarWrapper>
+    </Sidebar>
+</div>
