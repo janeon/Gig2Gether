34d6610a00743ccb8de3cd4f4dbfd5673838f2e8
Jane <janeon@cmu.edu>
2024-07-23 16:02:13 -0400

    figuring out titles

--- src/routes/protected/settings/account/+page.svelte
+++ src/routes/protected/settings/account/+page.svelte
@@ -2,13 +2,14 @@
     import { page } from "$app/stores";
     import { Label } from "flowbite-svelte";
 
-    let email = $page.data.user?.email
+    let username = $page.data.user?.username
+    export const title = "My Account"
 </script>
 
-<h1>My Account</h1>
+<h1>{title}</h1>
 <div>
-    <Label>My Email</Label>
-    <p>{email}</p>
+    <Label>My Email/Phone</Label>
+    <p>{username}</p>
 </div>
 <!-- <div>
     <Label>My Phone</Label>
