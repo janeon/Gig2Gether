24e8bb7fd5b50896b0bf8f536efa177351d1b9bd
Jane <janeon@cmu.edu>
2024-07-23 15:34:32 -0400

    setting layout for mobile pages

--- src/routes/protected/settings/+layout.svelte
+++ src/routes/protected/settings/+layout.svelte
@@ -1,11 +1,27 @@
-<script>
+<script lang="ts">
     import SettingsSidebar from "$lib/components/SettingsSidebar.svelte";
+    import { onMount } from 'svelte';
 
+    let mobile: boolean;
+    onMount(() => {
+      mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(window.navigator.userAgent);
+      console.log("mobile", mobile)
+      ;
+    });
 </script>
 
-<div class="flex flex-row">
+{#if mobile}
     <SettingsSidebar/>
-    <div class="p-8">
-        <slot/>
+    <div class="min-h-screen flex flex-col items-center p-4">
+        <div class="w-full max-w-sm p-6">
+            <slot/>
+        </div>
     </div>
-</div>
+{:else}
+    <div class="flex flex-row">
+        <SettingsSidebar/>
+        <div class="p-8">
+            <slot/>
+        </div>
+    </div>
+{/if}
