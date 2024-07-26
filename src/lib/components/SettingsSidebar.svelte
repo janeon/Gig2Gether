ee0f24a9a39e516121ebee18d17613bb5ba9f0fa
Jane <janeon@cmu.edu>
2024-07-25 23:08:21 -0400

    fix syntax

--- src/lib/components/SettingsSidebar.svelte
+++ src/lib/components/SettingsSidebar.svelte
@@ -1,8 +1,36 @@
 <script lang="ts">
     import { page } from '$app/stores';
-    import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
+    import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper} from 'flowbite-svelte';
 
     $: activeUrl = $page.url.pathname;
+    import { sineIn } from 'svelte/easing';
+    import '@fortawesome/fontawesome-free/css/all.min.css';
+
+    let mobile: boolean;
+    onMount(() => {
+      mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(window.navigator.userAgent);
+      // console.log("mobile", mobile)
+      ;
+    });
+    let hidden2 = true;
+    let transitionParams = {
+        x: -320,
+        duration: 200,
+        easing: sineIn
+    };
+    const closeDrawer = () => hidden2 = true;
+
+    let settings = [
+            { label: "My Account", href: "/protected/settings/account" },
+            { label: "My Worker Profile", href: "/protected/settings/profile" },
+            { label: "Demographics", href: "/protected/settings/demographics" },
+            { label: "Sharing Preferences", href: "/protected/settings/sharing_preferences" },
+            { label: "Withdraw Data", href:"/protected/settings/withdraw" },
+            { label: "Notification" }
+          ]
+
+    export let title: string;
+    
 </script>
 
 <div class="flex w-1/4">
