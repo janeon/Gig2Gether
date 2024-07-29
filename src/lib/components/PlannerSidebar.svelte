a4c14eff38dc3657b6151e2416ec1e3fc84f7eb9
Yeshua Ayala <a.yeshua@wustl.edu>
2024-07-29 12:49:52 -0400

    tax page

---
+++ src/lib/components/PlannerSidebar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+    import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
+
+    $: activeUrl = $page.url.pathname;
+</script>
+
+<Sidebar {activeUrl}>
+    <SidebarWrapper>
+        <SidebarGroup>
+            <SidebarItem label="Work" href="/protected/work-day"/>
+            <SidebarItem label="Tax" href="/protected/tax"/>
+          </SidebarGroup>
+    </SidebarWrapper>
+</Sidebar>
+
+
