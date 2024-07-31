1f6a61cc2f84225b6072d61e194d29296e72510b
Yeshua Ayala <a.yeshua@wustl.edu>
2024-07-31 18:33:34 -0400

    hourly data probe + fixing calender probe

---
+++ src/routes/protected/trends/collectiveInsights/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import TrendsSidebar from "$lib/components/TrendsSidebar.svelte";
+    import { Card } from "flowbite-svelte";
+</script>
+
+<div class="flex flex-row">
+    <div class="w-1/4 p-2">
+        <TrendsSidebar />
+    </div>
+
+    <div class="flex flex-col items-center w-3/4 p-2">
+
+        <div class = "justify-center">
+         
+        </div>
+
+        <div class = "w-90">
+      
+        </div>
+
+      
+        <div class = "justify-center mt-4">
+      
+        </div>
+    </div>
+</div>
