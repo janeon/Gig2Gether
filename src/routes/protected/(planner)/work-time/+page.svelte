491d0cd51df12c4362b0c1b66b25b710ec9b4605
Yeshua Ayala <a.yeshua@wustl.edu>
2024-07-31 11:21:57 -0400

    planner pages done

--- src/routes/protected/(planner)/work-time/+page.svelte
+++ src/routes/protected/(planner)/work-time/+page.svelte
@@ -2,6 +2,7 @@
     import Datepicker from "praecox-datepicker";
     import PlannerSidebar from "$lib/components/PlannerSidebar.svelte";
     import { goto } from "$app/navigation";
+    import ScheduleSelector from 'react-schedule-selector'
 
 
     async function nextPage() {
@@ -11,8 +12,14 @@
         goto('../protected/work-day')
     }
 
-</script>
+    let schedule = [];
 
+    const handleChange = (newSchedule) => {
+        // console.log(newSchedule);
+        schedule = newSchedule;
+    };
+
+</script>
 
 <div class="flex flex-row">
     <div class="w-1/4 p-2">
@@ -21,9 +28,21 @@
 
     <div class="flex flex-col items-center w-3/4 p-2">
 
-        
+        <div class = "justify-center">
+            <h1 class = "font-bold">When would you like to work?</h1>
+        </div>
 
+        <div class = "w-90">
+            <react:ScheduleSelector         
+            selection={schedule}
+            numDays={5}
+            minTime={8}
+            maxTime={22}
+            hourlyChunks={2}
+            onChange={handleChange}/>
+        </div>
 
+      
         <div class = "justify-center mt-4">
             <button
             class="bg-black text-white py-2 px-4 rounded"
@@ -38,3 +57,4 @@
         </div>
     </div>
 </div>
+
