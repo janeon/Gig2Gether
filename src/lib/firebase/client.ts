e3969d82ed87058e5c36402c7ab7636ed1bfd21e
Sajel Surati <sajelsurati@gmail.com>
2024-08-01 14:02:28 -0400

    Service worker is working!

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -3,6 +3,7 @@ import { initializeApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, RecaptchaVerifier, signInWithPhoneNumber, type Auth} from 'firebase/auth';
 import { getFirestore, type Firestore } from 'firebase/firestore';
 import { getStorage, type FirebaseStorage } from "firebase/storage";
+import { getMessaging, isSupported } from 'firebase/messaging';
 import { getAnalytics } from "firebase/analytics";
 
 let db: Firestore;
@@ -17,6 +18,7 @@ const firebaseConfig = {
  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+ messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID
 };
 
 // Initialize Firebase
@@ -26,6 +28,7 @@ app = initializeApp(firebaseConfig);
 auth = getAuth(app);
 db = getFirestore(app, "gigshare");
 storage = getStorage(app);
+let messaging = async () => await isSupported() && getMessaging(app)
 // const analytics = getAnalytics(app);
 
-export { RecaptchaVerifier, signInWithPhoneNumber, auth, db, app, storage};
+export { RecaptchaVerifier, signInWithPhoneNumber, auth, db, app, storage, messaging};
