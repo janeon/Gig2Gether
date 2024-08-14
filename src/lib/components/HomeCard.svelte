15bd864721097f831bc4fce2f27310f9cd96f2fa
Jane <janeon@cmu.edu>
2024-08-14 14:12:44 -0500

    make buttons blue

--- src/lib/components/HomeCard.svelte
+++ src/lib/components/HomeCard.svelte
@@ -46,7 +46,7 @@ const getButtonStyle = (index) => {
     {#each buttons as { label, route }, index}
       {#if label && route}
         <button class="text-xxs sm:text-xxs md:text-xs lg:text-sm py-1 px-2 rounded-md text-black font-medium w-full sm:w-auto"
-          style="background-color:rgb(217, 172, 147)"
+          style="background-color:rgb(208, 219, 234)"
           on:click={() => goto(route)}>
           {label}
         </button>
