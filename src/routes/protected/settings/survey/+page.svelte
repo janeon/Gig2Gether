abfd5e2b4d44ef70ec28450025787161780e2749
Jane <janeon@cmu.edu>
2024-08-20 05:25:08 -0400

    adding survey page

---
+++ src/routes/protected/settings/survey/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+	import { updateTitle } from '$lib/stores/title';
+    import { onMount } from 'svelte';
+    
+    onMount(() => {
+        updateTitle('Survey Page');    
+    });
+    
+    
+</script>
